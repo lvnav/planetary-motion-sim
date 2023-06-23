87cfdb238863301ba10f53913c9d0a6162519059
lthullier <l.thullier@otaree.com>
2023-06-23 19:03:34 +0200

    Add math helpers

---
+++ src/helpers/math.ts
@@ -0,0 +1,11 @@
+export function degToRad(degrees: number): number {
+  return (degrees * Math.PI) / 180;
+}
+
+export function radToDeg(radians: number): number {
+  return (radians * 180) / Math.PI;
+}
+
+export function sinPeriod(): number {
+  return Math.PI * 2;
+}
