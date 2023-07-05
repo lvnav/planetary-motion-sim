2f336c9914257cb789d36062f110e3b911df56cd
lthullier <l.thullier@otaree.com>
2023-07-05 20:59:48 +0200

    Add getDistanceFromCenter

--- src/helpers/debug.ts
+++ src/helpers/debug.ts
@@ -1,4 +1,4 @@
-import { Box3, Object3D } from "three";
+import { Box3, Object3D, Vector3 } from "three";
 import { type Coords } from "../interfaces/coords";
 
 export function getObjectDimensions(object: Object3D): Coords {
@@ -9,4 +9,8 @@ export function getObjectDimensions(object: Object3D): Coords {
     y: Math.abs(meshBounds.max.y - meshBounds.min.y),
     z: Math.abs(meshBounds.max.z - meshBounds.min.z),
   };
+}
+
+export function getDistanceFromCenter(object: Object3D): number {
+  return object.position.distanceTo(new Vector3(0, 0, 0));
 }
